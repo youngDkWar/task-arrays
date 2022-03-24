90145b6b025b3b232cde5005eadc6cf22f74a325
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-03-25 01:45:10 +0500

    task-arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,35 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    return data
+        .split(',')
+        .map((e) => +e)
+        .sort((a, b) => a - b)
+        .join(',');
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    return data.filter(function (n) {
+        return n < 100;
+    });
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+        result.push(array1[i]);
+        result.push(array2[i]);
+    }
+    return result;
+}
